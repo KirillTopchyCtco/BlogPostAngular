084aea077f9130abebd432b71c5c93590758371c
KirillTopchyCtco <kirill.topchy@ctco.lv>
2024-11-20 13:59:29 +0200

    formatting added

--- src/app/posts/state/post.effects.ts
+++ src/app/posts/state/post.effects.ts
@@ -1,24 +1,23 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { of } from "rxjs";
-import { catchError, map, mergeMap } from "rxjs/operators";
-import { PostService } from "../post.service";
-import { PostActions } from "./post.actions";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { PostService } from '../post.service';
+import { PostActions } from './post.actions';
 
 @Injectable()
 export class PostEffects {
-    constructor(
-        private actions$: Actions,
-        private postService: PostService
-    ) { }
+  constructor(private actions$: Actions, private postService: PostService) {}
 
-    loadBlogPosts$ = createEffect(() => {
-        return this.actions$.pipe(
-            ofType(PostActions.loadPosts),
-            mergeMap(() => this.postService.getPosts().pipe(
-                map(posts => PostActions.loadPostsSuccess({ posts: posts })),
-                catchError(error => of(PostActions.loadPostsFailure({ error })))
-            ))
+  loadBlogPosts$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(PostActions.loadPosts),
+      switchMap(() =>
+        this.postService.getPosts().pipe(
+          map((posts) => PostActions.loadPostsSuccess({ posts: posts })),
+          catchError((error) => of(PostActions.loadPostsFailure({ error })))
         )
-    })
+      )
+    );
+  });
 }
